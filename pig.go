5ca85c35212e5a07b746e8bee47bf19553f73c7c
gengwenyuan <gengwenyuan@gmail.com>
2013-07-24 22:30:55 +0800

    version 0.9.1 修改接口，改用json接口传递，包括用户名和命令两部分。
    Signed-off-by: gengwenyuan <gengwenyuan@gmail.com>

--- pig.go
+++ pig.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"net"
@@ -28,48 +30,52 @@ func main() {
 		}
 		go Play(conn)
 	}
-
 }
 
 func Play(conn net.Conn) {
 	defer conn.Close()
+	enc := json.NewEncoder(conn)
+	dec := json.NewDecoder(conn)
 again:
 	conn.Write([]byte("Game Begin\n"))
 	var players chan Player
 	players = make(chan Player, 2)
 	players <- &computer{"computer1", 0, 27}
 
-	conn.Write([]byte("----------------------------\ninput your name plase:"))
-	var buffer []byte
-	buffer = make([]byte, 3)
-	_, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println(err)
-	}
-	players <- &user{string(buffer), 0}
+	conn.Write([]byte("----------------------------\n"))
+	var player user
+	conn.Write([]byte("input your name plase: {\"Name\":\"gwy\"}\n"))
+	if err := dec.Decode(&player); err == io.EOF {
+		enc.Encode(fmt.Sprintf("err: %v", err))
+	} else if err != nil {
+		enc.Encode(fmt.Sprintf("err: %v", err))
+	} else {
+		player.Score = 0
+		players <- &player
+		for {
+			player := <-players
+			conn.Write([]byte(fmt.Sprintf("----------Now %s turn----------\n", player.GetName())))
+			score := player.Play(conn)
+			if score > WIN {
+				conn.Write([]byte(fmt.Sprintf("GAME OVER.\n %s win.\n", player.GetName())))
+				conn.Write([]byte(fmt.Sprintf("Play again? y/n\n")))
 
-	for {
-		player := <-players
-		conn.Write([]byte(fmt.Sprintf("----------Now %s turn----------\n", player.GetName())))
-		score := player.Play(conn)
-		if score > WIN {
-			conn.Write([]byte(fmt.Sprintf("GAME OVER.\n %s win.\n", player.GetName())))
-			conn.Write([]byte(fmt.Sprintf("Play again? y/n\n")))
-
-			var buffer []byte
-			buffer = make([]byte, 2)
-			_, err := conn.Read(buffer)
-			if err != nil {
-				fmt.Println(err)
-				break
+				var buffer []byte
+				buffer = make([]byte, 2)
+				_, err := conn.Read(buffer)
+				if err != nil {
+					fmt.Println(err)
+					break
+				}
+				if string(buffer) == "n\n" {
+					break
+				} else {
+					players <- player
+					goto again
+				}
 			}
-			if string(buffer) == "n\n" {
-				break
-			} else {
-				goto again
-			}
+			players <- player
 		}
-		players <- player
 	}
 }
 
@@ -105,48 +111,65 @@ func (player *computer) Play(conn net.Conn) int {
 	return player.Score
 }
 
+type CmdMessage struct {
+	CMD uint32 //ROLL or STAY
+}
+
+const (
+	ROLL = iota
+	STAY
+)
+
 type user struct {
-	Name  string
-	Score int
+	Name      string
+	Score     int
+	ThisTurn  int
+	RollScore int
 }
 
 func (player *user) GetName() string {
 	return player.Name
 }
 
+func (player *user) Roll() (end bool) {
+	end = false
+	player.RollScore = rand.Intn(6) + 1
+	if player.RollScore == 1 {
+		player.ThisTurn = 0
+		end = true
+	} else {
+		player.ThisTurn += player.RollScore
+	}
+	return
+}
 func (player *user) Play(conn net.Conn) int {
-
-	thisTurn := 0
-	var buffer []byte
-	buffer = make([]byte, 2)
-	for {
-		conn.Write([]byte(fmt.Sprintf("roll(r) or stay(s)?\n")))
-		buffer[0] = 0
-		_, err := conn.Read(buffer)
-		if err != nil {
-			fmt.Println(err)
+	dec := json.NewDecoder(conn)
+	enc := json.NewEncoder(conn)
+	end := false
+	player.ThisTurn = 0
+	player.RollScore = 0
+	for !end {
+		enc.Encode(player)
+		conn.Write([]byte(fmt.Sprintf("roll: {\"CMD\":0} or stay: {\"CMD\":1}?\n")))
+		var msg CmdMessage
+		if err := dec.Decode(&msg); err == io.EOF {
 			break
-		}
-		switch {
-		case string(buffer) == "r\n":
-			currentScore := rand.Intn(6) + 1
-			conn.Write([]byte(fmt.Sprintf("This turn %d, rolling... get %d.\n", thisTurn, currentScore)))
-			if currentScore == 1 {
-				thisTurn = 0
-				goto end
-			} else {
-				thisTurn += currentScore
+		} else if err != nil {
+			conn.Write([]byte(fmt.Sprintf("%v", err)))
+			log.Fatal("Error when decode: ", err)
+		} else {
+			switch msg.CMD {
+			case ROLL:
+				conn.Write([]byte(fmt.Sprintf("Rolling... \n")))
+				end = player.Roll()
+			case STAY:
+				end = true
+			default:
+				conn.Write([]byte(fmt.Sprintf("Invalid input, roll: {\"CMD\":0} or stay: {\"CMD\":1}?")))
 			}
-		case string(buffer) == "s\n":
-			goto end
-		default:
-			time.Sleep(time.Second)
-			conn.Write([]byte(fmt.Sprintf("Invalid input, roll(r) or stay(s)?\n")))
-			continue
 		}
 	}
-end:
-	player.Score += thisTurn
-	conn.Write([]byte(fmt.Sprintf("Finally, total score %d, this turn %d.\n", player.Score, thisTurn)))
+	player.Score += player.ThisTurn
+	enc.Encode(player)
 	return player.Score
 }
